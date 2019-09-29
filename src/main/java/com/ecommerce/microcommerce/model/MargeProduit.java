f6ca4df89d862434677cda60b024ed05258b94d4
air75 <laksariy@yahoo.fr>
2019-09-29 16:08:02 +0200

    ajout de cette class pour afficher le produit et la marge

---
+++ src/main/java/com/ecommerce/microcommerce/model/MargeProduit.java
@@ -0,0 +1,29 @@
+package com.ecommerce.microcommerce.model;
+
+public class MargeProduit {
+	
+	private Product product;
+	private int marge;
+	
+	
+	public MargeProduit() {
+	}
+	
+	
+	public Product getProduct() {
+		return product;
+	}
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+	public int getMarge() {
+		return marge;
+	}
+	public void setMarge(int marge) {
+		this.marge = marge;
+	}
+	
+	
+	
+
+}
